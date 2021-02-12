94eb5bf7793e531794431e70ab3de5291af21f21
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-02-12 14:56:37 +0300

    ts: build

---
+++ dist/hse.esm.js
@@ -0,0 +1,79 @@
+/**
+ * На вход подается натуральное четырехзначное число N,
+ * вывести YES, если сумма крайних цифр равна сумме средних. Вывести NO в противном случае.
+ * @param number {number}
+ */
+const task1 = number => {
+  const stringNumber = number.toString();
+
+  if (stringNumber.length === 4) {
+    const array = Array.from(stringNumber);
+    const outer = parseInt(array[0]) + parseInt(array[3]);
+    const inner = parseInt(array[1]) + parseInt(array[2]);
+    if (outer === inner) return 'YES';
+  }
+
+  return 'NO';
+};
+/**
+ * На вход подаются два числа: N и M.
+ * Напишите программу, которая умножает первое число на 5, если оно больше второго по абсолютной величине.
+ * @param n {number}
+ * @param m {number}
+ */
+
+const task2 = (n, m) => {
+  if (Math.abs(n) > Math.abs(m)) return [n * 5, m];
+  return [n, m];
+};
+/**
+ * На вход подается натуральное число N. Напишите программу вычисления выражения: 1 / (N * (N + 1))
+ * @param number {number}
+ */
+
+const task3 = number => {
+  let sum = 0;
+
+  for (let i = 1; i <= number; i++) {
+    sum += 1 / (i * (i + 1));
+  }
+
+  return parseFloat(sum.toString().slice(0, 5));
+};
+/**
+ * Даны значения одномерного массива размером 20 элементов.
+ * Преобразовать исходный массив, заменив нулями элементы между первым и вторым отрицательными.
+ * @param array {number[]}
+ */
+
+const task4 = array => {
+  const negatives = {
+    first: 0,
+    second: 0
+  };
+
+  if (Array.isArray(array) && array.length > 0) {
+    for (let i = 0; i < array.length; i++) {
+      //exit loop if negatives found
+      if (negatives.first !== 0 && negatives.second !== 0) break;
+
+      if (array[i] < 0) {
+        if (negatives.first === 0) {
+          negatives.first = i;
+        } else if (negatives.second === 0) {
+          negatives.second = i;
+        }
+      }
+    }
+  }
+
+  if (negatives.first && negatives.second) {
+    //rebuild original array via map
+    return array.map((n, i) => i > negatives.first && i < negatives.second ? 0 : n);
+  }
+
+  return array;
+};
+
+export { task1, task2, task3, task4 };
+//# sourceMappingURL=hse.esm.js.map
---
+++ dist/index.d.ts
@@ -0,0 +1,24 @@
+/**
+ * На вход подается натуральное четырехзначное число N,
+ * вывести YES, если сумма крайних цифр равна сумме средних. Вывести NO в противном случае.
+ * @param number {number}
+ */
+export declare const task1: (number: number) => string;
+/**
+ * На вход подаются два числа: N и M.
+ * Напишите программу, которая умножает первое число на 5, если оно больше второго по абсолютной величине.
+ * @param n {number}
+ * @param m {number}
+ */
+export declare const task2: (n: number, m: number) => number[];
+/**
+ * На вход подается натуральное число N. Напишите программу вычисления выражения: 1 / (N * (N + 1))
+ * @param number {number}
+ */
+export declare const task3: (number: number) => number;
+/**
+ * Даны значения одномерного массива размером 20 элементов.
+ * Преобразовать исходный массив, заменив нулями элементы между первым и вторым отрицательными.
+ * @param array {number[]}
+ */
+export declare const task4: (array: number[]) => number[];
